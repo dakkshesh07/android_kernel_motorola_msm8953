d55c5b4bacdb7c74fd956ff14b5ea49c0ea31979
Dakkshesh <dakkshesh5@gmail.com>
2022-12-23 18:18:18 +0530

    Revert "Kbuild: fix kallsyms failure when building with GCC LTO for arm64"
    This reverts commit 5ee7aefd8c812b1ce89ebab9fa72afcd7d767855.
    
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- scripts/kallsyms.c
+++ scripts/kallsyms.c
@@ -113,15 +113,13 @@ static int check_symbol_range(const char *sym, unsigned long long addr,
 static int read_symbol(FILE *in, struct sym_entry *s)
 {
 	char str[500];
-	char buf[LINE_MAX];
 	char *sym, stype;
 	int rc;
 
-	if (fgets(buf, sizeof(buf), in) == NULL) {
-		return -1;
-	}
-	rc = sscanf(buf, "%llx %c %499s\n", &s->addr, &stype, str);
-	if (rc < 3) {
+	rc = fscanf(in, "%llx %c %499s\n", &s->addr, &stype, str);
+	if (rc != 3) {
+		if (rc != EOF && fgets(str, 500, in) == NULL)
+			fprintf(stderr, "Read error or end of file.\n");
 		return -1;
 	}
 	if (strlen(str) > KSYM_NAME_LEN) {
@@ -152,13 +150,14 @@ static int read_symbol(FILE *in, struct sym_entry *s)
 			return -1;
 
 	}
-	else if (toupper(stype) == 'U')
+	else if (toupper(stype) == 'U' ||
+		 is_arm_mapping_symbol(sym))
 		return -1;
 	/* exclude also MIPS ELF local symbols ($L123 instead of .L123) */
 	else if (str[0] == '$')
 		return -1;
 	/* exclude debugging symbols */
-	else if (toupper(stype) == 'N')
+	else if (stype == 'N')
 		return -1;
 	/* exclude s390 kasan local symbols */
 	else if (!strncmp(sym, ".LASANPC", 8))
@@ -180,8 +179,7 @@ static int read_symbol(FILE *in, struct sym_entry *s)
 
 	/* Record if we've found __per_cpu_start/end. */
 	check_symbol_range(sym, s->addr, &percpu_range, 1);
-	if (toupper(stype) == 'W' && strstr(sym, ".c") != NULL)
-		return -1;
+
 	return 0;
 }
 
